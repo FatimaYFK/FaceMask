7f5302e4a4259805c968ad4fdcea8218276beb3b
Fadime Konuk <fadime.konuk@stud.fh-campuswien.ac.at>
2022-06-05 23:25:59 +0200

    Last Commit

--- app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
@@ -60,28 +60,18 @@ import org.tensorflow.lite.examples.detection.tracking.MultiBoxTracker;
 public class DetectorActivity extends CameraActivity implements OnImageAvailableListener {
   private static final Logger LOGGER = new Logger();
 
-  // Configuration values for the prepackaged SSD model.
-  //private static final int TF_OD_API_INPUT_SIZE = 300;
-  //private static final boolean TF_OD_API_IS_QUANTIZED = true;
-  //private static final String TF_OD_API_MODEL_FILE = "detect.tflite";
-  //private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/labelmap.txt";
-
-  //private static final Size DESIRED_PREVIEW_SIZE = new Size(640, 480);
-
   // Face Mask
   private static final int TF_OD_API_INPUT_SIZE = 224;
   private static final boolean TF_OD_API_IS_QUANTIZED = false;
   private static final String TF_OD_API_MODEL_FILE = "mask_detector.tflite";
   private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/mask_labelmap.txt";
-
   private static final DetectorMode MODE = DetectorMode.TF_OD_API;
+
   // Minimum detection confidence to track a detection.
   private static final float MINIMUM_CONFIDENCE_TF_OD_API = 0.5f;
   private static final boolean MAINTAIN_ASPECT = false;
-
   private static final Size DESIRED_PREVIEW_SIZE = new Size(800, 600);
-  //private static final int CROP_SIZE = 320;
-  //private static final Size CROP_SIZE = new Size(320, 320);
+
 
 
 
